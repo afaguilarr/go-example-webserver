87da99cde0e91d5010bc1c7f3f5e7628fe92ed5e
Los Payitos <los_payitos@Loss-MacBook-Pro.local>
2021-11-01 12:33:41 -0500

    Add correct code to check correct pylint run

--- test/functional/steps/http.py
+++ test/functional/steps/http.py
@@ -9,5 +9,5 @@ from functional.steps_context import StepsContext
 
 @then(parsers.parse('the response has a "{status_code}" status code'))
 def response_has_status_code(steps_context: StepsContext, status_code: str):
-    #"""Function to check that the response contains certain status code"""
+    """Function to check that the response contains certain status code"""
     assert_equals(int(status_code), steps_context.request.status_code)
