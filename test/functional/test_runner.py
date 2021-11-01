1db99c293562ac7cbc1f8ec67ef90de0b9523269
Los Payitos <los_payitos@Loss-MacBook-Pro.local>
2021-11-01 12:43:42 -0500

    Fix pytest execution

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -2,5 +2,8 @@
 # Third Party
 from pytest_bdd import scenarios
 
+# First Party
+from functional.steps import *  # needed import for pytest-bdd pylint: disable=W0614 disable=W0401
+
 # run all tests
 scenarios('features')
