01a6a98b7f2c4725d7ad9ef26d211f766babd788
Los Payitos <los_payitos@Loss-MacBook-Pro.local>
2021-11-14 13:55:06 -0500

    Add functional test connecting to the DB

--- test/functional/conftest.py
+++ test/functional/conftest.py
@@ -1,5 +1,7 @@
 """Module to add the common fixtures for functional tests"""
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
 from functional.steps_context import StepsContext
 
 
@@ -7,3 +9,12 @@ from functional.steps_context import StepsContext
 def steps_context() -> StepsContext:
     """Function to make the StepsContext object available for all steps"""
     return StepsContext()
+
+
+@pytest.fixture(scope="function", autouse=True)
+def session() -> Engine:
+    """Function to create the base state of the postgres DB"""
+    db_string = "postgresql://admin:admin@postgres:5432/hello_world"
+    db = create_engine(db_string)
+    db.execute("DELETE FROM hello_world") 
+    return db 
--- test/helpers/assertions.py
+++ test/helpers/assertions.py
@@ -5,3 +5,8 @@ from typing import Any
 def assert_equals(expected: Any, actual: Any):
     """Function to assert that 2 values are equal"""
     assert expected == actual, f"Expected\n{expected}\nto equal\n{actual}"
+
+
+def assert_not_none(expected: Any):
+    """xD"""
+    assert expected is not None, f"Expected\n{expected}\nto not equal\nNone"
