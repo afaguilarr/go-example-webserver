c7a886216356441c2d4a578ae50cd123cfa41d7b
Andres Aguilar <aaguilar@MacBook-Pro-de-IT.local>
2022-10-23 14:31:11 -0500

    fix actual docker webserver name

--- test/helpers/base_urls.py
+++ test/helpers/base_urls.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-webserver_name = os.getenv('DOCKER_PROJECT_NAME')
-WEBSERVER_BASE_URL = f"http://{webserver_name}_webserver_1:8080"
+webserver_name = os.getenv('DOCKER_WEBSERVER_NAME')
+WEBSERVER_BASE_URL = f"http://{webserver_name}:8080"
 WEBSERVER_BASE_URL_TRAILING_SLASH = WEBSERVER_BASE_URL + "/"
 HELLO_NAME_ENDPOINT = WEBSERVER_BASE_URL_TRAILING_SLASH + "name"
 HELLO_NAME_ENDPOINT_PARAM = HELLO_NAME_ENDPOINT + "/{}"
