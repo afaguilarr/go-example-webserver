d6e703b6606cd7e3cfa3712bb187c11b39f7239b
Andres Aguilar <aaguilar@MacBook-Pro-de-IT.local>
2022-10-23 14:24:23 -0500

    Add project name to env variables for CI runs

--- test/helpers/base_urls.py
+++ test/helpers/base_urls.py
@@ -1,6 +1,14 @@
 """Helpers module including all base URLs"""
+# Standard library
+import os
 
-WEBSERVER_BASE_URL = "http://go-example-webserver_webserver_1:8080"
+# Third Party
+from dotenv import load_dotenv
+
+load_dotenv()
+
+webserver_name = os.getenv('DOCKER_PROJECT_NAME')
+WEBSERVER_BASE_URL = f"http://{webserver_name}_webserver_1:8080"
 WEBSERVER_BASE_URL_TRAILING_SLASH = WEBSERVER_BASE_URL + "/"
 HELLO_NAME_ENDPOINT = WEBSERVER_BASE_URL_TRAILING_SLASH + "name"
 HELLO_NAME_ENDPOINT_PARAM = HELLO_NAME_ENDPOINT + "/{}"
