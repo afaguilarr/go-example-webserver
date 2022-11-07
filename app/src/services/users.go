7b939ea39757a687a607e68aeb00da00022633b4
Andres Aguilar <aaguilar@MacBook-Pro-de-IT.local>
2022-11-07 17:43:37 -0500

    Add implementation for Authenticate, RefreshAccessToken, and LogOut RPCs

--- app/src/services/users.go
+++ app/src/services/users.go
@@ -78,3 +78,74 @@ func (su *ServicesUsers) LogIn(ctx context.Context, req *proto.LogInRequest) (*p
 
 	return resp, nil
 }
+
+// Authenticate receives a JWT access token and its username and verifies
+// if the access token is valid.
+func (su *ServicesUsers) Authenticate(ctx context.Context, req *proto.AuthenticateRequest) (*proto.AuthenticateResponse, error) {
+	log.Println("Authenticate RPC was called!")
+
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "got nil value in the request")
+	}
+
+	if req.Username == "" {
+		return nil, status.Error(codes.InvalidArgument, "username can't be an empty string")
+	}
+
+	if req.AccessToken == "" {
+		return nil, status.Error(codes.InvalidArgument, "access token can't be an empty string")
+	}
+
+	resp, err := su.BusinessUsers.Authenticate(ctx, req)
+	if err != nil {
+		return nil, status.Errorf(status.Code(err), "while authenticating user: %s", err.Error())
+	}
+
+	return resp, nil
+}
+
+// RefreshAccessToken receives and verifies the correctness of a JWT refresh token,
+// and returns a new valid JWT access token.
+func (su *ServicesUsers) RefreshAccessToken(ctx context.Context, req *proto.RefreshAccessTokenRequest) (*proto.RefreshAccessTokenResponse, error) {
+	log.Println("RefreshAccessToken RPC was called!")
+
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "got nil value in the request")
+	}
+
+	if req.Username == "" {
+		return nil, status.Error(codes.InvalidArgument, "username can't be an empty string")
+	}
+
+	if req.RefreshToken == "" {
+		return nil, status.Error(codes.InvalidArgument, "refresh token can't be an empty string")
+	}
+
+	resp, err := su.BusinessUsers.RefreshAccessToken(ctx, req)
+	if err != nil {
+		return nil, status.Errorf(status.Code(err), "while refreshing access token for user: %s", err.Error())
+	}
+
+	return resp, nil
+}
+
+// LogOut receives the information of a user, and revokes its refresh token secret, so that a new
+// Login process has to be made in order to create a new refresh token secret.
+func (su *ServicesUsers) LogOut(ctx context.Context, req *proto.LogOutRequest) (*proto.LogOutResponse, error) {
+	log.Println("LogOut RPC was called!")
+
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "got nil value in the request")
+	}
+
+	if req.Username == "" {
+		return nil, status.Error(codes.InvalidArgument, "username can't be an empty string")
+	}
+
+	resp, err := su.BusinessUsers.LogOut(ctx, req)
+	if err != nil {
+		return nil, status.Errorf(status.Code(err), "while logging user out: %s", err.Error())
+	}
+
+	return resp, nil
+}
