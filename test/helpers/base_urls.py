835cd38459ee09b4e0649ba7298c58a15688af0b
Andres Felipe <andresfelipe@MacBook-Pro-de-Andres.local>
2022-12-02 13:58:48 -0500

    Add environment variables for Nginx

--- test/helpers/base_urls.py
+++ test/helpers/base_urls.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 webserver_name = os.getenv('DOCKER_WEBSERVER_NAME')
-WEBSERVER_BASE_URL = f"http://{webserver_name}:8080"
+WEBSERVER_BASE_URL = f"http://{webserver_name}1:8080"
 WEBSERVER_BASE_URL_TRAILING_SLASH = WEBSERVER_BASE_URL + "/"
 HELLO_NAME_ENDPOINT = WEBSERVER_BASE_URL_TRAILING_SLASH + "name"
 HELLO_NAME_ENDPOINT_PARAM = HELLO_NAME_ENDPOINT + "/{}"
